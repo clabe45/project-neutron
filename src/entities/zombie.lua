591d9f23109fc6092d047efcd48c8542cc1fcf57
taiz <tyler.walker.student@gmail.com>
2020-02-07 17:39:59 -0500

    Add zombie aggro behavior

--- src/entities/zombie.lua
+++ src/entities/zombie.lua
@@ -5,11 +5,26 @@ Zombie = {
 
 -- idle: When the zombie is standing still, do this
 function Zombie.idle(this)
+	this.dx = 0
 	Entities.jump(this)
+end
+
+-- aggro: What to do when the player is seen
+function Zombie.aggro(this)
+	-- Follow the player
+	if (Player.x > this.x) then
+		this.dx = 3
+	else
+		this.dx = -3
+	end
 end
 
 -- doBehaivor: Do different behaivors based on the state
 function Zombie.doBehaivor(this)
-	Zombie.idle(this)
+	if (math.abs(Player.x - this.x) < 200) then
+		Zombie.aggro(this)
+	else
+		Zombie.idle(this)
+	end
 end
 
