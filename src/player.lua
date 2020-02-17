4302903072039efab8960bbb11c10aae118e66dd
taiz <tyler.walker.student@gmail.com>
2020-02-17 17:56:51 -0500

    Fix offset camera errors

--- src/player.lua
+++ src/player.lua
@@ -6,18 +6,18 @@ Player = {
 	hitboxX = 25,
 	hitboxY = 50,
 	idleSprite = love.graphics.newImage("assets/char_sprites/MC.png"),
-	spriteOffsetX = -38,
-	spriteOffsetY = -40
+	spriteOffsetX = 18,
+	spriteOffsetY = 20
 }
 
 -- drawPlayer: Draws the player sprite
 function Player.drawPlayer()
 	-- The hitbox
 	love.graphics.setColor(0, 1, 0, .1)
-	--love.graphics.rectangle('fill', Camera.convert("x", Player.x), Camera.convert("y",Player.y), Player.hitboxX, Player.hitboxY)
+	love.graphics.rectangle('fill', Camera.convert("x", Player.x), Camera.convert("y",Player.y), Player.hitboxX, Player.hitboxY)
 	-- The graphic
 	love.graphics.setColor(1, 1, 1, 1)
 	-- The graphic is currently being blown up 2x, this results in some fuzziness
-	love.graphics.draw(Player.idleSprite, Player.x + Player.spriteOffsetX, Player.y + Player.spriteOffsetY, 0, 2, 2)
+	love.graphics.draw(Player.idleSprite, Camera.convert("x", Player.x), Camera.convert("y", Player.y), 0, 2, 2, Player.spriteOffsetX, Player.spriteOffsetY)
 end
 
