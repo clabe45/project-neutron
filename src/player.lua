b064f2b507d2eccc95a774e2f4b24bd168b4d20e
taiz <tyler.walker.student@gmail.com>
2020-02-17 17:29:29 -0500

    Add player spritecode

--- src/player.lua
+++ src/player.lua
@@ -4,14 +4,20 @@ Player = {
 	dx = 0,
 	dy = 0,
 	hitboxX = 25,
-	hitboxY = 50
+	hitboxY = 50,
+	idleSprite = love.graphics.newImage("assets/char_sprites/MC.png"),
+	spriteOffsetX = -38,
+	spriteOffsetY = -40
 }
 
 -- drawPlayer: Draws the player sprite
 function Player.drawPlayer()
-	love.graphics.setColor(0, 1, 0)
-	love.graphics.rectangle('fill', Camera.convert("x", Player.x), Camera.convert("y",Player.y), Player.hitboxX, Player.hitboxY)
-	--idlePlayer = love.graphics.newImage("idle.png")
-	--love.graphics.draw(idlePlayer, Player.x, Player.y)
+	-- The hitbox
+	love.graphics.setColor(0, 1, 0, .1)
+	--love.graphics.rectangle('fill', Camera.convert("x", Player.x), Camera.convert("y",Player.y), Player.hitboxX, Player.hitboxY)
+	-- The graphic
+	love.graphics.setColor(1, 1, 1, 1)
+	-- The graphic is currently being blown up 2x, this results in some fuzziness
+	love.graphics.draw(Player.idleSprite, Player.x + Player.spriteOffsetX, Player.y + Player.spriteOffsetY, 0, 2, 2)
 end
 
