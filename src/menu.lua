11b82fd0abad13e85ab9370ff2f717a5f343dab7
taiz <tyler.walker.student@gmail.com>
2020-02-28 15:58:36 -0500

    Add descriptions for menu items

--- src/menu.lua
+++ src/menu.lua
@@ -2,7 +2,14 @@ Menu = {
 	isActive = false,
 	page = "main",
 	maxSelection = 5,
-	selection = 1
+	selection = 1,
+	descFlavortext = {
+		"The things you wear!",
+		"The things you use!",
+		"The things you killed!",
+		"The things you can read!",
+		"The things you can change!"
+	}
 }
 
 -- drawMenu: Will later be used to draw a menu showing status
@@ -11,7 +18,7 @@ function Menu.drawMenu()
 		-- Draw main window
 		love.graphics.setColor(1, 1, 1, 1)
 		love.graphics.rectangle('fill', 0, 0, windowWidth, windowHeight)
-		Menu.drawElement("list", 200, windowHeight-160, 500, windowHeight-40, "IDK", 0)
+		Menu.drawElement("desc", 200, windowHeight-160, 500, 100, Menu.descFlavortext[Menu.selection], 0)
 		-- Draw list items
 		Menu.drawElement("list", 20, windowHeight-160, 150, 20, "Equipment", 1)
 		Menu.drawElement("list", 20, windowHeight-130, 150, 20, "Items", 2)
@@ -23,12 +30,16 @@ end
 
 -- drawElement: Draws an element to the menu
 function Menu.drawElement(elementType, x, y, width, height, text, index)
+	-- Draw the outer box
 	if (index == Menu.selection) then
+		-- Make the selected one colored
 		love.graphics.setColor(0, 0, .5, 1)
 	else
 		love.graphics.setColor(.5, .5, .5, 1)
 	end
 	love.graphics.rectangle('fill', x, y, width, height)
+
+	-- Draw the text
 	love.graphics.setColor(1, 1, 1, 1)
 	love.graphics.print(text, x, y)
 end
