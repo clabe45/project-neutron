77f76c8a76a9401aa0a69f5863cc7299bc5cadc0
taizweb <tyler.walker.student@gmail.com>
2020-03-06 11:29:19 -0500

    Make dialog print to console

--- src/dialog.lua
+++ src/dialog.lua
@@ -1,5 +1,6 @@
 Dialog = {
 	dialogLines = {},
+	index = 1,
 	currentSpeaker = nil,
 	currentDialog = nil,
 	isOpen = false
@@ -11,23 +12,32 @@ function Dialog.openDialog(id)
 		-- Load the speech file and split it
 		local rawDialog = love.filesystem.read("dialog/template.txt")
 		Dialog.dialogLines = split(rawDialog, "\n")
-
-		-- Loop through dialog lines and load speaker
-		for i=1,#Dialog.dialogLines do
-			-- Check for all the speakers
-			if (Dialog.dialogLines[i] == "[Hammer]") then
-				Dialog.currentSpeaker = "Hammer"
-			elseif (Dialog.dialogLines[i] == "[MC]") then
-				Dialog.currentSpeaker = "MC"
-			-- Strip blank lines
-			elseif (Dialog.dialogLines ~= "") then
-				Dialog.currentDialog = Dialog.dialogLines[i]
-				print(Dialog.currentSpeaker)
-				print(Dialog.currentDialog)
-			end
-		end
-	else
 		Dialog.isOpen = true
+	end
+	Dialog.nextLine()
+end
+
+-- nextLine: Get the next line in the dialog
+function Dialog.nextLine()
+	-- Loop through dialog lines and load speaker
+	for i=Dialog.index,#Dialog.dialogLines do
+		-- Check for all the speakers
+		if (Dialog.dialogLines[i] == "[Hammer]") then
+			Dialog.currentSpeaker = "Hammer"
+			print(Dialog.currentSpeaker)
+		elseif (Dialog.dialogLines[i] == "[MC]") then
+			Dialog.currentSpeaker = "MC"
+			print(Dialog.currentSpeaker)
+		-- Strip blank lines
+		elseif (Dialog.dialogLines[i] ~= "") then
+			Dialog.currentDialog = Dialog.dialogLines[i]
+			print(Dialog.currentDialog)
+			Dialog.index = i+1
+			return
+		else
+			-- For blank lines
+			Dialog.index = i+1
+		end
 	end
 end
 
